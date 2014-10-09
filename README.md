29b8bf0cba7b6be60ce2070c138c80953e642b11
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-10-09 01:54:29 -0500

    add JsonContext#fromFile

--- README.md
+++ README.md
@@ -5,15 +5,14 @@ Based on specs:
  * [JSON Reference](http://tools.ietf.org/html/draft-pbryan-zyp-json-ref-03)
  * [JavaScript Object Notation (JSON) Pointer](http://tools.ietf.org/html/rfc6901)
 
+
 ```java
-ObjectMapper mapper = new ObjectMapper();
+File file = new File("src/test/resources/nest.json");
 
-JsonNode node = mapper.readTree(new File("src/test/resources/nest.json"));
-
-JsonContext context = new JsonContext();
-context.setNode(node);
-context.setPath("src/test/resources");
+JsonContext context = JsonContext.fromFile(file);
 JsonReference.process(context);
+JsonNode node = context.getNode();
 
+ObjectMapper mapper = new ObjectMapper();
 mapper.writeValue(new File("out.json"), node);
 ```
--- src/main/java/me/andrz/jackson/JsonContext.java
+++ src/main/java/me/andrz/jackson/JsonContext.java
@@ -2,6 +2,9 @@ package me.andrz.jackson;
 
 import com.fasterxml.jackson.databind.*;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  *
  */
@@ -12,9 +15,42 @@ public class JsonContext {
 
     public JsonContext() {}
 
+    /**
+     * No path context, may not be able to resolve file-relative references.
+     *
+     * @param node
+     */
+    public JsonContext(JsonNode node) {
+        this.node = node;
+    }
+
+    /**
+     *
+     * @param node
+     * @param path
+     */
     public JsonContext(JsonNode node, String path) {
         this.node = node;
         this.path = path;
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    public static JsonContext fromFile(File file) throws IOException {
+        JsonContext context = new JsonContext();
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(file);
+        context.setNode(node);
+
+        String parent = file.getParent();
+        context.setPath(parent);
+
+        return context;
     }
 
     public JsonNode getNode() {
@@ -33,10 +69,4 @@ public class JsonContext {
         this.path = path;
     }
 
-    public JsonContext clone() {
-        JsonContext context = new JsonContext();
-        context.setNode(node);
-        context.setPath(path);
-        return context;
-    }
 }
