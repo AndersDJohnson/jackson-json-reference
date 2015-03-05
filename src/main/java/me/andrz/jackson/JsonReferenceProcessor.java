3309615963f2157a9aad444a59806756c236e484
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-03-04 20:40:45 -0600

    Migrate to slf4j from log4j. Fixes #5

--- src/main/java/me/andrz/jackson/JsonReferenceProcessor.java
+++ src/main/java/me/andrz/jackson/JsonReferenceProcessor.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,7 +22,7 @@ import java.util.Set;
  */
 public class JsonReferenceProcessor {
 
-    private static final Logger logger = LogManager.getLogger(JsonReferenceProcessor.class);
+    private static final Logger logger = LoggerFactory.getLogger(JsonReferenceProcessor.class);
 
     private ObjectMapper mapper;
     private int maxDepth = 1;
